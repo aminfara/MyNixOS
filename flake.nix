5ab2375b5b6771e32a7962fcf06c9ec3bf3a8166
Ali 🚶 <ali.aminfar@gmail.com>
2026-05-12 00:16:07 +1000

    use unstable channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for my personal nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
