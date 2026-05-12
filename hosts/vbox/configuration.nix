2bcf9e2c3563aa95b4d1e2d045a6f3277da180f4
Ali 🚶 <ali.aminfar@gmail.com>
2026-05-12 23:29:45 +1000

    changed bootloader and potential graphical load

--- hosts/vbox/configuration.nix
+++ hosts/vbox/configuration.nix
@@ -1,36 +1,64 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./vboxsf.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot"; # Ensure this matches your actual mount point
+    };
+    grub = {
+      enable = true;
+      device = "nodev"; # Use "nodev" for UEFI systems
+      efiSupport = true;
+      # Optional: Add a theme
+      # theme = pkgs.stdenv.mkDerivation {
+      #   pname = "distro-grub-themes";
+      #   version = "3.1";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "AdisonCavani";
+      #     repo = "distro-grub-themes";
+      #     rev = "v3.1";
+      #     hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+      #   };
+      #   installPhase = "cp -r customize/nixos $out";
+      # };
+    };
+  };
+  # Optional: Enable Plymouth for a graphical boot experience
+  # boot.plymouth = {
+  #   enable = true;
+  #   theme = "bgrt"; # This is the standard UEFI logo theme
+  #   # themePackages = [ pkgs.adi1090x-plymouth ]; # Optional: extra fancy themes
+  # };
+  # boot.kernelParams = [
+  #   "quiet"
+  #   "splash"
+  #   "boot.shell_on_fail"
+  #   "loglevel=3"
+  #   "rd.systemd.show_status=false"
+  #   "rd.udev.log_level=3"
+  #   "udev.log_priority=3"
+  # ];
+  # boot.consoleLogLevel = 0;
+  # boot.initrd.verbose = false;
 
   networking.hostName = "vbox"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Australia/Sydney";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_AU.UTF-8";
     LC_IDENTIFICATION = "en_AU.UTF-8";
@@ -43,41 +71,27 @@
     LC_TIME = "en_AU.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
-    variant = "";
+    variant = "mac";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  programs.fish.enable = true;
+  programs.firefox.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+  ];
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Services that you want to have running on your system.
+
+  services.openssh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ali = {
@@ -94,42 +108,6 @@
     ];
   };
 
-  # Install fish shell.
-  programs.fish.enable = true;
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -138,8 +116,4 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
 }
